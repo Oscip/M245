55c51492775da6d7235892b81437762a2f6db329
Oscip <oscar@peachy.ch>
2026-03-18 16:00:35 +0100

    Fix base path for deployment v3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react' // Ändere dies von '@vitejs/react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/m245/',
+    base: '/M245/',
 })
